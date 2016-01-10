2f34c4eaa10b5b64ae685bbd72ce1e227ccb9b30
Sebastian Graf <sgraf1337@gmail.com>
2016-01-10 23:39:32 +0100

    extracting the polys works, but don't know how to go about triangulate that

--- visibility/src/main/java/visibility/osm/OSMGeometryParser.java
+++ visibility/src/main/java/visibility/osm/OSMGeometryParser.java
@@ -2,19 +2,18 @@ package visibility.osm;
 
 import com.ximpleware.*;
 import javafx.geometry.Point2D;
+import org.jooq.lambda.tuple.Tuple;
+import org.jooq.lambda.tuple.Tuple2;
 import visibility.types.GeometryParser;
 import visibility.types.Polygon;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Hashtable;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
 
 public class OSMGeometryParser implements GeometryParser {
     private final AutoPilot BUILDING_MULTIPOLYGON_PATH = new AutoPilot();
     private final AutoPilot BUILDING_WAY_PATH = new AutoPilot();
-    private final AutoPilot MEMBER_WAY_REF_PATH = new AutoPilot();
+    private final AutoPilot MEMBER_WAY_PATH = new AutoPilot();
     private final AutoPilot NODE_REF_PATH = new AutoPilot();
     private final AutoPilot WAY_PATH = new AutoPilot();
     private final AutoPilot NODE_PATH = new AutoPilot();
@@ -24,8 +23,8 @@ public class OSMGeometryParser implements GeometryParser {
             NODE_PATH.selectXPath("/osm/node");
             NODE_REF_PATH.selectXPath("nd/@ref");
             BUILDING_WAY_PATH.selectXPath("/osm/way[./tag[@k='building']]");
-            BUILDING_MULTIPOLYGON_PATH.selectXPath("/osm/relation[@type='multipolygon' and ./tag[@k='building']]");
-            MEMBER_WAY_REF_PATH.selectXPath("member[@type='way']/@ref");
+            BUILDING_MULTIPOLYGON_PATH.selectXPath("/osm/relation[./tag[@k='type' and @v='multipolygon'] and ./tag/@k='building']");
+            MEMBER_WAY_PATH.selectXPath("member[@type='way']");
             WAY_PATH.selectXPath("/osm/way");
         } catch (XPathParseException e) {
             e.printStackTrace();
@@ -42,14 +41,18 @@ public class OSMGeometryParser implements GeometryParser {
             NODE_REF_PATH.bind(vn);
             BUILDING_WAY_PATH.bind(vn);
             BUILDING_MULTIPOLYGON_PATH.bind(vn);
-            MEMBER_WAY_REF_PATH.bind(vn);
+            MEMBER_WAY_PATH.bind(vn);
             WAY_PATH.bind(vn);
 
             // A hash from node ids to actual node positions
             Hashtable<Long, Point2D> nodes = new Hashtable<>();
+            // A hash from way refs referenced from multipolygons to their actual list of node refs
+            Hashtable<Long, List<Long>> ways = new Hashtable<>();
 
             // The following call initializes accessed node refs in nodes to a dummy value.
             List<List<Long>> buildingWays = extractWaysOfBuildings(vn, nodes);
+            // The following call initializes accessed way refs from multipolygons to a dummy value.
+            List<List<Tuple2<WayRole, Long>>> multipolygons = extractWayRefsOfMultipolygons(vn, ways);
             // This will extract all referenced nodes, but no more.
             extractReferencedNodes(vn, nodes);
             // Finally build the polygon list by following the node refs in wayRefs.
@@ -67,7 +70,7 @@ public class OSMGeometryParser implements GeometryParser {
             for (Long ref : way) {
                 outerFace.add(nodes.get(ref));
             }
-            polygons.add(new Polygon(outerFace, new ArrayList<List<Point2D>>()));
+            polygons.add(new Polygon(outerFace, new ArrayList<>()));
         }
         return polygons;
     }
@@ -97,7 +100,7 @@ public class OSMGeometryParser implements GeometryParser {
         for (int i = BUILDING_WAY_PATH.evalXPath(); i != -1 ; i = BUILDING_WAY_PATH.evalXPath()) {
             // The lambda will put in a dummy value for each encountered node,
             // so that we know later which nodes we need to parse.
-            ways.add(extractRefs(vn, NODE_REF_PATH, (Long ref) -> nodes.put(ref, Point2D.ZERO)));
+            ways.add(extractNodeRefs(vn, NODE_REF_PATH, nodes));
         }
 
         vn.pop();
@@ -108,17 +111,17 @@ public class OSMGeometryParser implements GeometryParser {
     }
 
     /**
-     * Requires vn to point to a sub element. From there it iterates over all matches of the ap.
-     * Calls onRef for visited nodes.
+     * Requires vn to point to a way element. From there it iterates over all matches of the ap.
+     * Puts a 0 point into nodes for every node it finds.
      */
-    private List<Long> extractRefs(VTDNav vn, AutoPilot ap, Function<Long, Object> onRef) throws NavException, XPathEvalException {
+    private List<Long> extractNodeRefs(VTDNav vn, AutoPilot ap, Hashtable<Long, Point2D> nodes) throws NavException, XPathEvalException {
         vn.push();
 
         List<Long> refs = new ArrayList<>();
         for (int j = ap.evalXPath(); j != -1; j = ap.evalXPath()) {
             long ref = Long.parseLong(vn.toString(j + 1));
             refs.add(ref);
-            onRef.apply(ref);
+            nodes.put(ref, Point2D.ZERO);
         }
         ap.resetXPath();
 
@@ -127,13 +130,13 @@ public class OSMGeometryParser implements GeometryParser {
         return refs;
     }
 
-    private List<List<Long>> extractWayRefsOfMultipolygons(VTDNav vn, Hashtable<Long, List<Long>> ways) throws NavException, XPathEvalException {
+    private List<List<Tuple2<WayRole, Long>>> extractWayRefsOfMultipolygons(VTDNav vn, Hashtable<Long, List<Long>> ways) throws NavException, XPathEvalException {
         vn.push();
 
-        List<List<Long>> multipolygons = new ArrayList<>();
+        List<List<Tuple2<WayRole, Long>>> multipolygons = new ArrayList<>();
         for (int i = BUILDING_MULTIPOLYGON_PATH.evalXPath(); i != -1 ; i = BUILDING_MULTIPOLYGON_PATH.evalXPath()) {
             // For an explanation for the lambda see extractWaysOfBuildings
-            multipolygons.add(extractRefs(vn, MEMBER_WAY_REF_PATH, (Long ref) -> ways.put(ref, Collections.emptyList())));
+            multipolygons.add(extractWayRefs(vn, MEMBER_WAY_PATH, ways));
         }
 
         vn.pop();
@@ -141,5 +144,47 @@ public class OSMGeometryParser implements GeometryParser {
         BUILDING_WAY_PATH.resetXPath();
 
         return multipolygons;
+    }
+
+    /**
+     * Requires vn to point to a relation element. From there it iterates over all matches of the ap.
+     * Puts an empty list for each referenced way.
+     */
+    private List<Tuple2<WayRole, Long>> extractWayRefs(VTDNav vn, AutoPilot ap, Hashtable<Long, List<Long>> ways) throws NavException, XPathEvalException {
+        vn.push();
+
+        List<Tuple2<WayRole, Long>> refs = new ArrayList<>();
+        for (int j = ap.evalXPath(); j != -1; j = ap.evalXPath()) {
+            long ref = Long.parseLong(vn.toString(vn.getAttrVal("ref")));
+            String roleAsString = vn.toString(vn.getAttrVal("role"));
+            WayRole role;
+            switch (roleAsString) {
+                case "inner":
+                    role = WayRole.INNER;
+                    break;
+                case "outer":
+                    role = WayRole.OUTER;
+                    break;
+                default:
+                    throw new RuntimeException("Parsing the role blew up");
+            }
+            refs.add(Tuple.tuple(role, ref));
+            ways.put(ref, Collections.emptyList());
+        }
+        ap.resetXPath();
+
+        vn.pop();
+
+        return refs;
+    }
+
+    /**
+     * Covers the 'role' attribute of a way within a multipolygon relation.
+     * OUTER would mean that the inner of the circumfered area belongs to the polygon,
+     * while INNER cuts holes into existing polygon areas.
+     */
+    private enum WayRole {
+        INNER,
+        OUTER
     }
 }
